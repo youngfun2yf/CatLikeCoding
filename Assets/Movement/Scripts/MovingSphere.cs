dc0b6bc544ca792eb792f9a77de0bc14546344a7
yangfan <yangfan@oasgames.com>
2021-01-06 17:30:08 +0800

    MovingSphere

---
+++ Assets/Movement/Scripts/MovingSphere.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 1f)] private float bounciness = .5f;
+    
+    [SerializeField, Range(0f, 100f)] private float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f;
+
+    [SerializeField] private Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+
+    private Vector3 _velocity;
+    
+    private void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+        _velocity.x = Mathf.MoveTowards(_velocity.x, desiredVelocity.x, maxSpeedChange);
+        _velocity.z = Mathf.MoveTowards(_velocity.z, desiredVelocity.z, maxSpeedChange);
+        Vector3 displacement = _velocity * Time.deltaTime;
+        Vector3 newPosition = transform.localPosition + displacement;
+
+        if (newPosition.x < allowedArea.xMin)
+        {
+            newPosition.x = allowedArea.xMin;
+            _velocity.x *= -bounciness;
+        }
+        else if (newPosition.x > allowedArea.xMax)
+        {
+            newPosition.x = allowedArea.xMax;
+            _velocity.x *= -bounciness;
+        }
+        if (newPosition.z < allowedArea.yMin)
+        {
+            newPosition.z = allowedArea.yMin;
+            _velocity.z *= -bounciness;
+        }
+        else if (newPosition.z > allowedArea.yMax)
+        {
+            newPosition.z = allowedArea.yMax;
+            _velocity.z *= -bounciness;
+        }
+
+        transform.localPosition = newPosition;
+    }
+}
